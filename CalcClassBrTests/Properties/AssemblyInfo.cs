9560749f573072b1cc64d4fb3657fb42c495a4ea
bezil <95500334+nazarpodubinskyi@users.noreply.github.com>
2022-11-21 20:46:56 +0200

    Add project files.

---
+++ CalcClassBrTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CalcClassBrTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CalcClassBrTests")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ff15468b-11fa-4461-a7d7-436984b03a58")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ CalcClassBrTests/UnitTest1.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Xml;
+
+namespace CalcClassBr.Tests
+{
+    [TestClass]
+    public class CalcClassTests
+    {
+        public TestContext TestContext { get; set; }
+
+       
+        [TestMethod]
+        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.XML", @"C:\Program Files\Calculator1\CalcClassBrTests\TestData.xml", "data", DataAccessMethod.Sequential)]
+        public void TestMultiplicateMethodFromCalculator()
+        {
+            //Arrange
+            long a = long.Parse(TestContext.DataRow["a"].ToString());
+            long b = long.Parse(TestContext.DataRow["b"].ToString());
+            long expected = long.Parse(TestContext.DataRow["expected"].ToString());
+
+            //Act
+            var result = CalcClass.Mult(a, b);
+            var reversed = CalcClass.Mult(b, a);
+
+            //Assert
+            Assert.AreEqual(expected, result);
+            Assert.AreEqual(expected, reversed);
+        }
+    }
+}//max 
